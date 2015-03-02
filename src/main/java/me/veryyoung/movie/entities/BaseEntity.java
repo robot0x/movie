4820c52177070fa2ed49db2b55cc68b59573f922
veryyoung <codingyoung@gmail.com>
2015-03-02 18:05:54 +0800

    add lombok

--- src/main/java/me/veryyoung/movie/entities/BaseEntity.java
+++ src/main/java/me/veryyoung/movie/entities/BaseEntity.java
@@ -1,8 +1,11 @@
 package me.veryyoung.movie.entities;
 
+import lombok.Data;
+
 /**
  * Created by veryyoung on 2015/3/2.
  */
+@Data
 public class BaseEntity {
 
     private String id;
--- src/main/java/me/veryyoung/movie/entities/User.java
+++ src/main/java/me/veryyoung/movie/entities/User.java
@@ -1,9 +1,12 @@
 package me.veryyoung.movie.entities;
 
+import lombok.Data;
+
 /**
  * Created by veryyoung on 2015/3/2.
  */
 
+@Data
 public class User extends BaseEntity {
 
     private String userName;
