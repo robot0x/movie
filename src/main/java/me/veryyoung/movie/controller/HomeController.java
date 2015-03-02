1a8da432e89248efb762439b5f65c440100cb522
veryyoung <codingyoung@gmail.com>
2015-03-02 18:41:40 +0800

    springmvc demo

--- src/main/java/me/veryyoung/movie/controller/HomeController.java
+++ src/main/java/me/veryyoung/movie/controller/HomeController.java
@@ -9,4 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController extends BaseController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "/index";
+    }
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <title>首页</title>
+</head>
+<body>
+<h1>这是首页</h1>
+
+</body>
+</html>
