fa32ac8064df9ccf1da3c911fb06281befeccf3c
veryyoung <codingyoung@gmail.com>
2015-03-03 16:29:15 +0800

    add schema

---
+++ src/main/resources/schema/movie.sql
@@ -0,0 +1,20 @@
+
+
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE database movie;
+
+use movie;
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` varchar(20) NOT NULL,
+  `userName` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `isAdmin` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
