9a8427806749b8af3cfb94a85d3cf7bacd9cd521
veryyoung <codingyoung@gmail.com>
2015-03-18 15:02:28 +0800

    add subjectController

---
+++ src/main/java/me/veryyoung/movie/controller/SubjectController.java
@@ -0,0 +1,14 @@
+package me.veryyoung.movie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by veryyoung on 2015/3/18.
+ */
+@Controller
+@RequestMapping("/subject")
+public class SubjectController extends BaseController {
+
+
+}
