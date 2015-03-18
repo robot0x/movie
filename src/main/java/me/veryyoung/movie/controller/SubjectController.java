dffbf133789a5afcff42fb8e8207f50dd556f4cd
veryyoung <codingyoung@gmail.com>
2015-03-18 15:29:00 +0800

    add getSuject

--- src/main/java/me/veryyoung/movie/controller/SubjectController.java
+++ src/main/java/me/veryyoung/movie/controller/SubjectController.java
@@ -1,7 +1,11 @@
 package me.veryyoung.movie.controller;
 
+import me.veryyoung.movie.entity.Subject;
+import me.veryyoung.movie.utils.DoubanUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by veryyoung on 2015/3/18.
@@ -10,5 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/subject")
 public class SubjectController extends BaseController {
 
+    @RequestMapping("/{id}")
+    @ResponseBody
+    public Subject getSubject(@PathVariable(value = "id") String id) {
+        return DoubanUtils.getSubject(id);
+    }
 
 }
