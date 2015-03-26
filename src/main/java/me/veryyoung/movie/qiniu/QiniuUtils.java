0c0f6a6a61b6632bb93ff943f65efca399b5f90f
veryyoung <codingyoung@gmail.com>
2015-03-26 16:19:36 +0800

    add 7niuyun

---
+++ src/main/java/me/veryyoung/movie/qiniu/QiniuUtils.java
@@ -0,0 +1,40 @@
+package me.veryyoung.movie.qiniu;
+
+import com.qiniu.api.auth.AuthException;
+import com.qiniu.api.auth.digest.Mac;
+import com.qiniu.api.io.PutRet;
+import com.qiniu.api.resumableio.ResumeableIoApi;
+import com.qiniu.api.rs.PutPolicy;
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by veryyoung on 2015/3/26.
+ */
+public class QiniuUtils {
+
+
+    private static Logger logger = LoggerFactory.getLogger(QiniuUtils.class);
+
+    private static final String ACCESS_KEY = "1OcsILqPu9A_YrO7bgAEBowPWwmjTfzt_zUoINRC";
+
+    private static final String SECRET_KEY = "BW1s2xfqoty1RRzNI4xhVMs6dt6i7zjf3FdbX9Ty";
+
+    private static final String BUCKET_NAME = "movie";
+
+    private void uploadStream(FileInputStream fis) throws AuthException, JSONException, FileNotFoundException {
+        Mac mac = new Mac(SECRET_KEY, SECRET_KEY);
+        PutPolicy putPolicy = new PutPolicy(BUCKET_NAME);
+        String upToken = putPolicy.token(mac);
+        PutRet ret = ResumeableIoApi.put(fis, upToken, null, null);
+        logger.info("ret:{}", ret);
+    }
+}
+
+
+
+
