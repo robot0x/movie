099fc6eb4784b2fe955c866ca85cd5617e1ba953
veryyoung <codingyoung@gmail.com>
2015-05-05 14:57:48 +0800

    add subject details

--- src/main/java/me/veryyoung/movie/controller/SubjectController.java
+++ src/main/java/me/veryyoung/movie/controller/SubjectController.java
@@ -1,12 +1,11 @@
 package me.veryyoung.movie.controller;
 
-import me.veryyoung.movie.entity.Subject;
 import me.veryyoung.movie.service.DoubanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by veryyoung on 2015/3/18.
@@ -19,9 +18,10 @@ public class SubjectController extends BaseController {
     private DoubanService doubanService;
 
     @RequestMapping("/{id}")
-    @ResponseBody
-    public Subject getSubject(@PathVariable(value = "id") String id) {
-        return doubanService.find(id);
+    public ModelAndView getSubject(@PathVariable(value = "id") String id) {
+        ModelAndView modelAndView = new ModelAndView("/subject/details");
+        modelAndView.addObject("subject", doubanService.find(id));
+        return modelAndView;
     }
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/subject/details.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>${subject.title}-${appName}</title>
+    <%@include file="../common/head.jspf" %>
+</head>
+<body>
+<%@include file="../common/topNav.jspf" %>
+
+
+<%@include file="../common/footer.jspf" %>
+
+
+</body>
+</html>
